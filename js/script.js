aa7b84972e14426f1829d2237adea65f018d37da
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-02-23 11:52:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // scripts.js - hello
 // welcome to my page
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name); //
 console.log('Hello, ' + name);
 /* Lorem ipsum dolor sit amet, consectetur adipiscing elit. */
