b83810928758d3ee88a5ea68787ee76b1a481684
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-02-23 11:47:00 +0100

    Add basic poject structure

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// scripts.js - hello
+// welcome to my page
+var name = prompt('Enter your name');
+alert('Hello, ' + name);
+console.log('Hello, ' + name);
+/* Lorem ipsum dolor sit amet, consectetur adipiscing elit. */
